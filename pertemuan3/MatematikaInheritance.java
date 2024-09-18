7a5a9ad45b4113c8abfe2607ecfb11ace76bba43
mochammadfadli21072004 <2310631170161@student.unsika.ac.id>
2024-09-18 14:36:04 +0700

    Add files via upload

---
+++ pertemuan3/MatematikaInheritance.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan3;
+
+/**
+ *
+ * @author 23106
+ */
+public class MatematikaInheritance {
+
+   
+    public static void main(String[] args) {
+    Matematika2 hasil = new Matematika2();
+
+    System.out.println("Pertambahan 20+10: "+hasil.pertambahan(20,10));
+    System.out.println("Pengurangan 10-5: "+hasil.pengurangan(10,5));
+    System.out.println("Perkalian 10*3: "+hasil.perkalian(10,3));
+    System.out.println("Pembagian 21/2: "+hasil.pembagian(21,2));
+    System.out.println("Modulus 3%4: "+hasil.modulus(3,4));
+        
+
+        
+
+    }
+    
+}
